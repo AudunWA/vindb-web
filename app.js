83ede2c7f1c104b76ff652041d4e52d4b4ef5932
Audun Arbo <audunwar@student.hist.no>
2016-06-22 13:34:14 +0200

    Added bower + dependencies

--- app.js
+++ app.js
@@ -33,7 +33,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Set up static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components',  express.static(path.join(__dirname, '/bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
