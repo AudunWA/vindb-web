b5a51dfbd4a35d0b2b067e00f7045d9ec91197da
Audun Arbo <audunwar@student.hist.no>
2016-06-25 17:15:22 +0200

    Added port environment variable, disabled dotenv for AWS production test.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-// Load environment variables from .env
-require('dotenv').config();
-
 // Global debug flag
 debug = typeof v8debug === 'object';
 
+//if (debug) {
+// Load environment variables from .env
+//  require('dotenv').config();
+//}
+
+var http = require('http');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -22,12 +25,16 @@ var app = express();
 
 // set mysql config
 pool = mysql.createPool({
-    connectionLimit: 15,
-    host     : process.env.RDS_HOSTNAME,
-    user     : process.env.RDS_USERNAME,
-    password : process.env.RDS_PASSWORD,
-    port     : process.env.RDS_PORT
+  connectionLimit: 15,
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
+  database: process.env.RDS_DB_NAME
 });
+
+// Listen port
+app.set('port', process.env.PORT || 3000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,7 +55,7 @@ app.use('/products', products)
 app.use('/history', history)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -59,7 +66,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -70,7 +77,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
@@ -78,5 +85,10 @@ app.use(function(err, req, res, next) {
   });
 });
 
+
+// Start listen
+http.createServer(app).listen(app.get('port'), function () {
+  console.log("Express server listening on port " + app.get('port'));
+});
 
 module.exports = app;
