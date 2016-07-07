053e6cf674dc6fab97c3f7a3e780573a848b06dd
Audun Arbo <audunwar@student.hist.no>
2016-07-07 15:47:10 +0200

    Started using squel for /history

--- routes/history.js
+++ routes/history.js
@@ -4,25 +4,42 @@ var format = require('date-format');
 var squel = require("squel");
 
 router.get('/', function (req, res, next) {
-  var query = "SELECT pc.product_id, p.varenavn, field.display_name, pc.old_value, pc.new_value, cl.time FROM product_change pc INNER JOIN field USING(field_id) INNER JOIN change_log cl USING(change_id) INNER JOIN product p ON (p.varenummer = pc.product_id)";
-  var parameters = [];
+  var squelQuery = squel.select({ autoQuoteAliasNames: false })
+    .field("pc.product_id")
+    .field("p.varenavn")
+    .field("field.display_name")
+    .field("pc.old_value")
+    .field("pc.new_value")
+    .field("cl.time")
+    .from("product_change pc")
+    .join("field", null, "field.field_id = pc.field_id")
+    .join("change_log", "cl", "cl.change_id = pc.change_id")
+    .join("product", "p", "p.varenummer = pc.product_id")
+    .order("time", false);
 
   // Date range
   if (req.query.start_date) {
     if (req.query.end_date) {
-      query += " WHERE DATE(cl.time) BETWEEN ? AND ? ORDER BY time DESC";
-      parameters.push(req.query.start_date);
-      parameters.push(req.query.end_date);
+      squelQuery
+        .where("DATE(cl.time) BETWEEN ? AND ?", req.query.start_date, req.query.end_date)
+        .limit(1000); // Workaround for really big page
     }
   }
   else {
-    query += " ORDER BY time DESC LIMIT 200";
+    squelQuery.limit(200);
   }
+
+  if (req.query.field_type) {
+    squelQuery.where("pc.field_id = ?", req.query.field_type);
+  }
+
+  //console.log(squelQuery.toString());
 
   // Get history from db
   pool.getConnection(function (err, connection) {
     if (err) throw err;
-    connection.query(query, parameters, function (err, rows, fields) {
+    var query = squelQuery.toParam();
+    connection.query(query.text, query.values, function (err, rows, fields) {
       connection.release();
 
       //getPageCount(connection, function(pageCount) {
